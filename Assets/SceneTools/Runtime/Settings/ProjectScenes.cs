8f8e5d124bc0ba1692968fb2256a30eef90d0cad
Yuri Sokolov <7556847+migus88@users.noreply.github.com>
2025-02-28 09:34:52 +0200

    formatting

--- Assets/SceneTools/Runtime/Settings/ProjectScenes.cs
+++ Assets/SceneTools/Runtime/Settings/ProjectScenes.cs
@@ -5,7 +5,7 @@ namespace SceneTools.Runtime.Settings
     public class ProjectScenes : ScriptableObject
     {
         [field: SerializeField] public string[] Names { get; set; }
-        [field:SerializeField] public int[] Indexes { get; set; }
-        [field:SerializeField] public string[] Addressables { get; set; }
+        [field: SerializeField] public int[] Indexes { get; set; }
+        [field: SerializeField] public string[] Addressables { get; set; }
     }
 }
